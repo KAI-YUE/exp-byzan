7fc5e8770b1bdee956e63f4d66974203aa266131
kyue <eric.kaiyue@gmail.com>
2023-07-27 19:52:13 -0400

    update

--- main.py
+++ main.py
@@ -90,34 +90,34 @@ def main():
     # # aggregators = ["mean"]
     num_attackers = [2, 6, 10, 14]
 
-    for user_data_mapping in user_data_mappings:
-        for aggregator in aggregators:
-            for num_att in num_attackers:
-                config.radius = r
-                config.user_data_mapping = user_data_mapping
-                # config.attacker_model = attacker
-                config.aggregator = aggregator
+    # for user_data_mapping in user_data_mappings:
+    #     for aggregator in aggregators:
+    #         for num_att in num_attackers:
+    #             # config.radius = r
+    #             config.user_data_mapping = user_data_mapping
+    #             # config.attacker_model = attacker
+    #             config.aggregator = aggregator
 
-                config.num_attackers = num_att
-                config.ipm_multiplier = (config.total_users-num_att)/num_att
+    #             config.num_attackers = num_att
+    #             config.ipm_multiplier = (config.total_users-num_att)/num_att
 
 
-                output_dir = init_outputfolder(config)
-                logger = init_logger(config, output_dir, config.seed)
+    output_dir = init_outputfolder(config)
+    logger = init_logger(config, output_dir, config.seed)
 
-                record = init_record(config)
+    record = init_record(config)
 
-                if config.device == "cuda":
-                    torch.backends.cudnn.benchmark = True
+    if config.device == "cuda":
+        torch.backends.cudnn.benchmark = True
 
-                start = time.time()
-                federated_learning(config, logger, record)
-                end = time.time()
+    start = time.time()
+    federated_learning(config, logger, record)
+    end = time.time()
 
-                logger.info("{:.3} mins has elapsed".format((end-start)/60))
-                save_record(record, output_dir)
+    logger.info("{:.3} mins has elapsed".format((end-start)/60))
+    save_record(record, output_dir)
 
-                logger.handlers.clear()
+    logger.handlers.clear()
 
 if __name__ == "__main__":
     main()
